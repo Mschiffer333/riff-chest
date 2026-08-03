7059a440616b38abcbcc66ab985ee2b9353dad03
Marco Bermúdez <marcus333_@hotmail.com>
2026-08-02 21:34:47 -0600

    chore(backend): configure ESLint and Prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-    res.status(200).json({
-        message: 'Hello from Riffchest API',
-        version: '1.0.0',
-        status: 'running'
-    });
+  res.status(200).json({
+    message: 'Hello from Riffchest API',
+    version: '1.0.0',
+    status: 'running',
+  });
 });
 
 export default app;
